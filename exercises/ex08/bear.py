67846b0731696f476239b3b0f5a0afaf5b79ac7b
Yanchen Xie <xyc@email.unc.edu>
2023-11-14 00:02:47 -0500

    fee

--- exercises/ex08/bear.py
+++ exercises/ex08/bear.py
@@ -2,8 +2,13 @@
 
 class Bear:
     
-    def __init__(self):
-        return None
+    age: int
+    hunger_score: int
+
+    def __init__(self, input_age: int = 0, input_hunger_score: int = 0) -> None:
+        """Constructs a bear."""
+        self.age = input_age
+        self.hunger_score = input_hunger_score 
     
     def one_day(self):
         return None
--- exercises/ex08/fish.py
+++ exercises/ex08/fish.py
@@ -2,7 +2,10 @@
 
 class Fish:
     
-    def __init__(self):
+    age: int
+
+    def __init__(self, input_age: int = 0):
+        self.age = input_age
         return None
     
     def one_day(self):
